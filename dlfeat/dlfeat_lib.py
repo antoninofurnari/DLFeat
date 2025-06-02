a13c2787251780f7031f180e7b38a5b39f4ec25e
Antonino Furnari <antonino.furnari@gmail.com>
2025-06-02 11:26:18 +0200

    fix 2 fit

--- dlfeat/dlfeat_lib.py
+++ dlfeat/dlfeat_lib.py
@@ -610,7 +610,7 @@ class DLFeatExtractor(BaseEstimator, TransformerMixin):
         return self.processor(processed_input_data, sampling_rate=self.target_sr, return_tensors="pt", padding=True)
 
     @torch.no_grad()
-    def fit(self, X):
+    def fit(self, X, y=None):
         pass
 
     @torch.no_grad()
